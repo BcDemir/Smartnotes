8c3aad6a44400baba59688ad72865a5d2ebd152b
BcDemir <burakcdemir@gmail.com>
2024-01-30 17:05:46 -0500

    feat: login and logout pages created

--- home/views.py
+++ home/views.py
@@ -3,9 +3,22 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import UserCreationForm
 
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+    
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
